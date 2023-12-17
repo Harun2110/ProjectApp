ec2dd12a85d161c854213350a90d1b9c300d3a65
Harun2110 <114378700+Harun2110@users.noreply.github.com>
2023-12-17 21:28:08 +0700

    Add project files.

---
+++ CobaProject/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using CobaProject.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaProject.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DestinationViewCommand { get; set; }
+        public RelayCommand MapsViewCommand { get; set; }
+        public RelayCommand SouvenirViewCommand { get; set; }
+        public RelayCommand TransactionViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DestinationViewModel DestinationVM { get; set; }
+        public MapsViewModel MapsVM { get; set; }
+        public SouvenirViewModel SouvenirVM {  get; set; }
+        public TransactionViewModel TransactionVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            DestinationVM = new DestinationViewModel();
+            MapsVM = new MapsViewModel();
+            SouvenirVM = new SouvenirViewModel();
+            TransactionVM = new TransactionViewModel();
+            SettingsVM = new SettingsViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DestinationViewCommand = new RelayCommand(o => {
+                CurrentView = DestinationVM;
+            });
+
+            MapsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MapsVM;
+            });
+
+            SouvenirViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SouvenirVM;
+            });
+
+            TransactionViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TransactionVM;
+            });
+
+            SettingsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsVM;
+            });
+        }
+    }
+}
---
+++ CobaProject/UserLogin.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CobaProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class UserLogin : Window
+    {
+        public UserLogin()
+        {
+            InitializeComponent();
+        }
+
+        public bool IsDarkTheme { get; set; }
+        private readonly PaletteHelper paletteHelper = new PaletteHelper();
+
+        private void toggleTheme(object sender, RoutedEventArgs e)
+        {
+            ITheme theme = paletteHelper.GetTheme();
+
+            if (IsDarkTheme = theme.GetBaseTheme() == BaseTheme.Dark)
+            {
+                IsDarkTheme = false;
+                theme.SetBaseTheme(Theme.Light);
+            }
+            else
+            {
+                IsDarkTheme = true;
+                theme.SetBaseTheme(Theme.Dark);
+            }
+            paletteHelper.SetTheme(theme);
+        }
+
+        private void exitApp(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var Username = txtUserName.Text;
+            var Password = txtPassword.Password;
+
+            using (UserDataContext context = new UserDataContext())
+            {
+                bool userfound = context.Users.Any(user => user.Name == Username && user.Password == Password);
+                if (userfound)
+                {
+                    GrantAccess();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("User Not Found");
+                }
+            }
+        }
+
+        public void GrantAccess()
+        {
+            MainPage main = new MainPage();
+            main.Show();
+        }
+    }
+}
